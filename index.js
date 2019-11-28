6f0f0eb54cfe057cee7c8ef2addb0c00d9ac13c9
geewey <geewey@gmail.com>
2019-11-28 00:10:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const sum = (previous, current) => previous + current;
+const totalBatteries = batteryBatches.reduce(sum);
